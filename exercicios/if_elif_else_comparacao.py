20db5945084e3eb197e8aef3d03e0e5833553120
Gabriela Furst <gabifurst2@gmail.com>
2024-06-18 19:48:53 -0300

    Atualização final 18/06/24

---
+++ exercicios/if_elif_else_comparacao.py
@@ -0,0 +1,9 @@
+primeiro_valor = input('Digite um numero: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor < segundo_valor:
+    print('O primeiro valor é menor que o segundo valor!')
+elif segundo_valor < primeiro_valor:
+    print('O segundo valor é menor que o primeiro valor!')
+else:
+    print('Os valores são equivalentes!')            
