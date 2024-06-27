a1edf87a018219aacf93ce47e7745ed4216f3260
Gabriela Furst <gabifurst2@gmail.com>
2024-06-27 14:50:41 -0300

    Todas as atualizações

--- exercicios/calculo_imc.py
+++
@@ -1,6 +0,0 @@
-nome = 'Gabriela Furst'
-altura = 1.60
-peso = 48
-imc = peso / altura ** 2
-
-print(f'Meu nome é {nome} minha altura é {altura} e meu peso é {peso}, baseado nessas informações meu imc é {imc}')
